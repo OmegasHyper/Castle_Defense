ddd6a24745c17a2e293fb47962fceb64f6188554
David-Amir-18 <davidamir1808@gmail.com>
2025-06-10 22:09:33 +0300

    removed file

--- sprites/map/castle.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="castle" tilewidth="320" tileheight="256" tilecount="1" columns="1">
- <image source="castle.png" trans="000000" width="320" height="256"/>
-</tileset>
