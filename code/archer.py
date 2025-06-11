d1f9c4b57222ed74c30e82ad591971de4ec6af75
Mohamed Abdelrazek <killer.omega2005@gmail.com>
2025-06-12 01:22:30 +0300

    archer class main methods implementation

---
+++ code/archer.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__(self):
+        pass
+
+class Queue:
+    def __init__(self):
+        pass
+
+class archer:
+    def __init__(self, state):
+        pass
+    def handle_shooting(self):
+        pass
+    def load(self):
+        pass
+    def update(self):
+        pass
