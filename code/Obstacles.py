22b18413e42da43327b17d4e64ec647b25655805
karim <karim2282004@gmail.com>
2025-06-14 22:27:57 +0300

    obstacles added by gold buy
    to put obstacle should pay to it
    solved problems in collision

--- code/Obstacles.py
+++ code/Obstacles.py
@@ -1,13 +1,16 @@
 import pygame as pg 
+import game
 class Obstacles(pg.sprite.Sprite) :
     image = pg.image.load("../sprites/Obstacles/obstacle.png")
-    imsc = pg.transform.smoothscale_by(image,(0.3,0.3))
+    # imsc = pg.transform.smoothscale_by(image,(0.3,0.3))
+    imsc = image
+    price = 200
     def __init__(self,groups,pos):
         super().__init__(groups)
         self.image = Obstacles.imsc
         self.rect = self.image.get_frect(center=pos)
         self.health = 100
-        self.hitbox = self.rect.inflate(-140,-180)
+        self.hitbox = self.rect.inflate(-200,-200)
         self.hitbox.top += 10
         self.health = 40
         self.obst = True
@@ -20,7 +23,8 @@ class Obstacles(pg.sprite.Sprite) :
             self.kill_obst()
 
 def put_obst(all_spr,Obst_spr,stack): 
-        if pg.mouse.get_just_pressed()[0]:
+        if pg.mouse.get_just_pressed()[0] and  game.gold_quantity >Obstacles.price:
+            game.gold_quantity -= Obstacles.price 
             mouse_pos = pg.mouse.get_pos()
             world_pos = pg.Vector2(mouse_pos) - all_spr.offset
             stack.push(Obstacles((all_spr, Obst_spr),world_pos))
